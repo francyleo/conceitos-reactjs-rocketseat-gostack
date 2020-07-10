5a978bd1b9abcd28a8d577ac607a3b9a5ef920b0
Francyleo Matos <fmatos@fingerprint.com.br>
2020-07-09 22:34:16 -0300

    adicionando repositório no usando o backend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepository] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepository(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      title: `Titulo do repositório ${Date.now()}`,
+      url: "https://github.com/francyleo/conceitos-reactjs-rocketseat-gostack",
+      techs: ["ReactJs"],
+    });
+
+    const repository = response.data;
+
+    setRepository([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
@@ -14,13 +31,14 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repo) => (
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
